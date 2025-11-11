8ad406595872b1154262d6e51bee385cb1e28859
Junichi Motohisa <motohisa@ist.hokudai.ac.jp>
2025-11-11 23:14:06 +0900

    shell script modified

--- makefindroot.sh
+++ makefindroot.sh
@@ -4,7 +4,7 @@ ORIG=gslfindroot
 # ORIG=gslmultiroot
 FUNCTION_NAME=$1
 
-cat $(ORIG)_func_skel.c | sed s/func_skel/$(FUNCION_NAME)/g > gslfindroot_$(FUNCTION_NAME).c
-cat $(ORIG)_func_skel.h | sed s/func_skel/$(FUNCION_NAME)/g > gslfindroot_$(FUNCTION_NAME).h
+cat ${ORIG}_func_skel.c | sed s/func_skel/${FUNCTION_NAME}/g > gslfindroot_${FUNCTION_NAME}.c
+cat ${ORIG}_func_skel.h | sed s/func_skel/${FUNCTION_NAME}/g > gslfindroot_${FUNCTION_NAME}.h
 
 
